e9eb84b8f101296b773dbe442353a88eede60434
Radgar123 <38327161+Radgar123@users.noreply.github.com>
2021-06-12 23:23:59 +0200

    Create Spawn Controller and tags
    Create spawn controller, tags: Agent, Follow and Create Agent Object

---
+++ AnimalShelter/Assets/Scripts/SpawnController.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnController : MonoBehaviour
+{
+    [Header("Spawn Agents Manager")]
+
+    [Range(2, 10)]
+    [SerializeField] private int timeToSpawn = 2;
+    [SerializeField] private int maxNumbersOfAgents = 30;
+    public static int actualNumberOfAgents;
+    public List<Transform> agentSpawnPoints;
+    public GameObject AgentPrefab;
+
+    [Header("Spawn Point To Follow")]
+
+    public List<Transform> followingPoints;
+    public GameObject followPrefab;
+    public GameObject followPrefabTemp = null;
+
+    private void Start()
+    {
+        StartCoroutine(SpawnAgentsInSeconds());
+        StartCoroutine(SpawnFollowingPointInSeconds());
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void SpawnAgentsInPoints()
+    {
+        if (actualNumberOfAgents <= maxNumbersOfAgents)
+        {
+            int spawnPointID = Random.Range(0, agentSpawnPoints.Count);
+            Instantiate(AgentPrefab, agentSpawnPoints[spawnPointID].position, agentSpawnPoints[spawnPointID].rotation);
+            actualNumberOfAgents++;
+        }
+    }
+
+    public void SpawnFollowingPointInPoints()
+    {
+        if (followPrefabTemp != null)
+        {
+            Destroy(followPrefabTemp);
+            int followPointID = Random.Range(0, followingPoints.Count);
+            Instantiate(followPrefab, followingPoints[followPointID].position, followingPoints[followPointID].rotation);
+        }
+        else
+        {
+            int followPointID = Random.Range(0, followingPoints.Count);
+            Instantiate(followPrefab, followingPoints[followPointID].position, followingPoints[followPointID].rotation);
+            followPrefabTemp = GameObject.FindWithTag("Follow");
+        }
+    }
+
+    IEnumerator SpawnAgentsInSeconds()
+    {
+        SpawnAgentsInPoints();
+        yield return new WaitForSeconds(timeToSpawn);
+        StartCoroutine(SpawnAgentsInSeconds());
+    }
+
+    IEnumerator SpawnFollowingPointInSeconds()
+    {
+        SpawnFollowingPointInPoints();
+        yield return new WaitForSeconds(5);
+        StartCoroutine(SpawnFollowingPointInSeconds());
+    }
+}
